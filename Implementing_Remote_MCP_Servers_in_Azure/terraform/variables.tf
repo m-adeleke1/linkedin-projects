4e4ce6cdac5f85eca3e423a8e5b1dc1dc53e239b
m-adeleke1 <101224140+m-adeleke1@users.noreply.github.com>
2025-05-25 22:03:36 -0400

    Update variables.tf
    Setting up the variables for my MCP Server project

--- Implementing_Remote_MCP_Servers_in_Azure/terraform/variables.tf
+++ Implementing_Remote_MCP_Servers_in_Azure/terraform/variables.tf
@@ -1,1 +1,26 @@
+variable "resource_group_name" {
+  type    = string
+  default = "mcp-rg"
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "vm_name" {
+  type    = string
+  default = "mcp-vm"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "azureuser"
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
+  description = "Password for the VM admin user"
+}
 
