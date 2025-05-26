20fbb79b936f7a478260e2ec23fe5a9452eb9e0d
m-adeleke1 <101224140+m-adeleke1@users.noreply.github.com>
2025-05-25 22:05:19 -0400

    Update outputs.tf
    Output values for my MCP Server project

--- Implementing_Remote_MCP_Servers_in_Azure/terraform/outputs.tf
+++ Implementing_Remote_MCP_Servers_in_Azure/terraform/outputs.tf
@@ -1,1 +1,14 @@
+output "vm_public_ip" {
+  description = "Public IP address of the VM"
+  value       = azurerm_public_ip.public_ip.ip_address
+}
+
+output "admin_username" {
+  value = var.admin_username
+}
+
+output "admin_password" {
+  value     = var.admin_password
+  sensitive = true
+}
 
